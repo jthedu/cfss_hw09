1c2d49438497f01c8bd253b8ac11c2bbea7acd18
Julia Du <duj@uchicago.edu>
2021-03-13 17:40:11 -0600

    data-hw09.R: created scraping script outside of .Rmd since Daily Mail updates frequently & don't wanna run this code in .Rmd (& get diff data) when knitting

---
+++ data_hw09.R
@@ -0,0 +1,28 @@
+# Scraping data for Daily Mail/Meghan Markle analysis
+# (aka for hw09.Rmd)
+
+# doing this in a separate script since Daily Mail constantly updates site
+
+# create folder to store local copies of df
+dir.create("./data")
+
+mail <- read_html("https://www.dailymail.co.uk/tvshowbiz/meghan-markle/index.html")
+
+mail_nodes <- html_nodes(mail, ".sport span , #content p, .linkro-ccow, .linkro-darkred a")
+
+mail_vec <- mail_nodes %>%
+  html_text()
+
+mail_vec <- 
+  mail_vec[c(-1, -400)] %>%
+  append(c("", ""), after = 2) 
+#adding spaces so that each article is divided into 5 elements
+
+almost_mail <- mail_vec %>%
+  split(ceiling(seq_along(mail_vec)/5)) %>%
+  as_tibble() # make a df
+
+# store local copy of dataframe
+almost_mail %>%
+  write_csv("./data/mail_210310.6pm.csv") 
+# this file name is specific to when I scraped the website
